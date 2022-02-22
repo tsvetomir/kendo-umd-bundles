53a43184a3decd5e66e76f65c30ddd5efaf3883f
ipeshev <ivan.g.peshev@gmail.com>
2022-02-22 15:59:28 +0200

    chore: add grid

--- sample-app/src/app/app.module.ts
+++ sample-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { GridModule } from '@progress/kendo-angular-grid';
 
 @NgModule({
     declarations: [
@@ -9,7 +10,8 @@ import { ButtonsModule } from '@progress/kendo-angular-buttons';
     ],
     imports: [
         BrowserModule,
-        ButtonsModule
+        ButtonsModule,
+        GridModule
     ],
     providers: [],
     bootstrap: [
